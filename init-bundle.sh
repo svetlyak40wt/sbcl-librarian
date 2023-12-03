6cf6123f46a357eec8f524c869eaa3fd757d0aad
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-12-03 09:23:00 +0000

    Add script init-bundle.

---
+++ init-bundle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p bundle
+cd bundle
+
+git clone https://github.com/joaotavora/sly
+git clone https://github.com/sionescu/bordeaux-threads
+git clone https://gitlab.common-lisp.net/alexandria/alexandria
+git clone https://github.com/lmj/global-vars
+git clone https://github.com/trivial-features/trivial-features
+git clone https://github.com/trivial-garbage/trivial-garbage
+
